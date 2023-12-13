e4436d75c9fc9ee188672e3bb3d36331135c64a1
TranDucDuyAnh <tranducduyanh.2018@gmail.com>
2023-12-13 21:33:03 +0700

    Update 13/12/23

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
+window.onscroll = function () {keepHeader() };
+
+// Get the header
+var header_title = document.getElementsByClassName("header-title");
+var header_menu = document.getElementsByClassName("header-menu");
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function keepHeader() {
+    if (window.scrollY > 250) {
+        for (var i = 0, len = header_title.length; i < len; i++) {
+            header_title[i].classList.add("header-title-stick");
+            header_menu[i].classList.add("header-menu-stick");
+        }
+    } else {
+        for (var i = 0, len = header_title.length; i < len; i++) {
+            header_title[i].classList.remove("header-title-stick");
+            header_menu[i].classList.remove("header-menu-stick");
+        }
+    }
+}
+
 var image_index = 1
 
 function bannerImageNext() {
@@ -24,17 +45,37 @@ function bannerChange() {
 
     image_banner.src = "./public/image/background-slider-0" + image_index + ".jpg"
 
+    banner_title.style.opacity = 0;
+    banner_title.style.y = '-100px'
+
     // Loop for opacity
-    var op = 0
+    var banner_op = 0
+
     var fade_timer = setInterval(function () {
-        if (op >= 1){
+        if (banner_op >= 1) {
             clearInterval(fade_timer);
         }
-        image_banner.style.opacity = op;
-        image_banner.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += 0.01;
-    }, 4);
-    
+        image_banner.style.opacity = banner_op;
+        image_banner.style.filter = 'alpha(opacity=' + banner_op * 100 + ")";
+        banner_op += 0.01;
+    }, 10);
+
+    setTimeout(function () {
+        var text_op = 0
+        var text_pos = -100
+
+        var fade_timer = setInterval(function () {
+            if (text_op >= 1) {
+                clearInterval(fade_timer);
+            }
+            banner_title.style.opacity = text_op;
+            banner_title.style.top = text_pos + 'px'
+            banner_title.style.filter = 'alpha(opacity=' + text_op * 100 + ")";
+            text_op += 0.01;
+            text_pos += 1;
+        }, 3);
+    }, 600)
+
     switch (image_index) {
         case 1:
             banner_title.innerText = "Farm Fresh Vegetables & Foods 100% Organic."
