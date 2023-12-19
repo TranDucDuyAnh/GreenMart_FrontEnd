8abd9644d4f1212ba2fdf3b6c94835309e7ee201
TranDucDuyAnh <tranducduyanh.2018@gmail.com>
2023-12-19 21:15:22 +0700

    Update 19/12/23

--- public/js/detail.js
+++ public/js/detail.js
@@ -19,5 +19,20 @@ function itemcountUpdate() {
     let price_update = document.getElementById("product-js-price")
 
     number_count.innerHTML = item_count
-    price_update.innerHTML = '$' + (99/2 * item_count).toFixed(2)
+    price_update.innerHTML = '$' + (99 * item_count).toFixed(2)
+}
+
+function itemimgUpdate(image) {
+    let item_image = document.getElementsByClassName("product-image-main")
+    item_image[0].src = image.src
+
+    let img_preview = document.getElementsByClassName("product-image-small")
+    for (var i = 0; i < img_preview[0].childNodes.length; i++) {
+        if (img_preview[0].childNodes[i].nodeType != 3) {
+            img_preview[0].childNodes[i].style.border = "1px solid var(--gray-light)"
+            if (img_preview[0].childNodes[i] == image) {
+                img_preview[0].childNodes[i].style.border = "1px solid var(--primary-color)"
+            }
+        }
+    }
 }
